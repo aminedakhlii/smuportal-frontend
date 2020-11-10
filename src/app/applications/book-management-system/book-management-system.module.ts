0e8f977d0159be94f271767a625c3b9998dc31d3
Mootez <mootez.saad@medtech.tn>
2020-11-10 15:55:50 +0100

    Add initial component for listing books

--- src/app/applications/book-management-system/book-management-system.module.ts
+++ src/app/applications/book-management-system/book-management-system.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from "@angular/router";
+import { ListBooksComponent } from './list-books/list-books.component';
 
-
+const routes: Routes = [
+  {
+    path: "list",
+    component: ListBooksComponent
+  },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [ListBooksComponent],
   imports: [
     CommonModule
   ]
